fa2b688451a663909aa6134db49ecd4ee923f3f2
zubair-hossain <technik52@arrowtec.de>
2021-08-20 02:44:04 +0200

    intigrated angular http client to call authenticated api

--- app/src/app/services/login.service.ts
+++ app/src/app/services/login.service.ts
@@ -1,15 +1,27 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { User } from "../interfaces/user";
+import { endPoints } from "../lib/endPoints";
 import { LOGGED_IN_USER } from "../mock/mock-user";
 
 @Injectable({
   providedIn: "root",
 })
 export class LoginService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+  private url = endPoints.AUTHENTICATION;
 
   authenticate(email: string, password: string): User {
-
+    this.url = this.url + encodeURI(email);
+    const body = {
+      password: password,
+      meta: "",
+    };
+    this.httpClient
+      .put<any>(this.url, body, { observe: "body", responseType: "json" })
+      .subscribe((data) => {
+        console.log(data);
+      });
     return LOGGED_IN_USER;
   }
 }
