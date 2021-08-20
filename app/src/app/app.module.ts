1ce57a2dc64f1e7253a5e9b808336ea594343f90
zubair-hossain <technik52@arrowtec.de>
2021-08-21 00:48:22 +0200

    Added auction component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
@@ -6,15 +6,18 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LoginPageComponent } from "../app/components/login-page/login-page.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { OverviewComponent } from "./components/overview/overview.component";
+import { AuctionComponent } from './components/auction/auction.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPageComponent],
+  declarations: [AppComponent, LoginPageComponent, OverviewComponent, AuctionComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ app/src/app/components/auction/auction.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-auction",
+  templateUrl: "./auction.component.html",
+  styleUrls: ["./auction.component.scss"],
+})
+export class AuctionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  timer = 12345;
+}
